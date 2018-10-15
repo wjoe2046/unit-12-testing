8e3b00464283e2cecf7c61636e04c07945fc6794
Joe Thel <jgraythel@gmail.com>
2018-10-15 16:58:17 -0700

    ✨ rebuild supertest for markets

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -3,45 +3,47 @@ const request = require('supertest');
 const PORT = process.env.PORT || 3000;
 const HOST = `http://localhost:${PORT}`;
 
+// These tests won't work unless your server is running!
 describe('Route integration', () => {
   describe('/', () => {
     describe('GET', () => {
+      // Note that we return the evaluation of `request` here! It evaluates to
+      // a promise, so Jest knows not to say this test passes until that
+      // promise resolves. See https://jestjs.io/docs/en/asynchronous
       it('responds with 200 status and text/html content type', () => request(HOST)
         .get('/')
         .expect('Content-Type', /text\/html/)
-        .expect(200));
+        .expect(300));
     });
   });
 
-  describe('/games', () => {
+  describe('/markets', () => {
     describe('GET', () => {
-      xit('response with 200 status and application/json content type', () => {
-      });
+      xit('responds with 200 status and application/json content type', () => {});
 
-      xit('games from appropriate json file in server/db/ are in body of response', () => {
-        // You'll need to inspect the body of the response and ensure it
-        // contains the games list. Might need to read the games json file in
-        // first to make sure the games in the response match the games in the
-        // database.
-      });
+      // For this test, you'll need to inspect the body of the response and
+      // ensure it contains the markets list. Check the markets.dev.json file
+      // in the dev database to get an idea of what shape you're expecting.
+      xit('markets from "DB" json are in body of response', () => {});
     });
 
     describe('POST', () => {
-      xit('responds to valid request with 200 status and application/json content type', () => {
-      });
+      xit('responds to valid request with 201 status and application/json content type', () => {});
 
-      xit('responds to a valid request with the item that was created in the DB', () => {
-        // Hint: inspect the response body and make sure it contains the
-        // winner, createdAt, and id fields.
-      });
+      // Hint: you should expect 5 fields on this object
+      xit('responds to valid request with the item that was created in the DB', () => {});
 
-      xit('responds to invalid request with 400 status and error message in body', () => {
-        // This feature is not fully implemented yet. Follow test driven
-        // development here! See description in readme. Hint: An invalid
-        // request is a POST request in which the POST body does not contain a
-        // JSON object with a "winner" key, or if the body contains fields
-        // other than "winner"
-      });
+      xit('responds to invalid request with 400 status and error message in body', () => {});
+    });
+
+    describe('PATCH', () => {
+      xit('responds to valid request with 200 status and application/json content type', () => {});
+
+      xit('responds to valid request with the item that was updated in the DB', () => {});
+
+      // This feature is yet implemented. Follow TDD here! See description in
+      // readme.
+      xit('responds to invalid request with 400 status and error message in body', () => {});
     });
   });
 });
