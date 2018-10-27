aec55feba0add46579c1fc2f3ba44f1f92bf5fa2
Joe Thel <jgraythel@gmail.com>
2018-10-27 11:48:11 -0700

    rewrite server to simpler functionality

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -1,25 +1,20 @@
-/* eslint arrow-body-style: 0 */
 const request = require('supertest');
+
 const server = require('../server');
 
 /**
- * Read the docs!!!!
- * https://www.npmjs.com/package/supertest
+ * Read the docs! https://www.npmjs.com/package/supertest
  */
 describe('Route integration', () => {
   afterAll(() => {
-    // Once these tests are done, we can close down the server. (Check out
-    // server/index.js. It exports the app.listen call from Express. This
-    // returns a server instance, which we can shut down whenever we like.)
     server.close();
   });
 
-  // These tests won't work unless your server is running!
   describe('/', () => {
     describe('GET', () => {
       // Note that we return the evaluation of `request` here! It evaluates to
-      // a promise, so Jest knows to wait until it resolves.
-      // See https://jestjs.io/docs/en/asynchronous
+      // a promise, so Jest knows not to say this test passes until that
+      // promise resolves. See https://jestjs.io/docs/en/asynchronous
       it('responds with 200 status and text/html content type', () => {
         return request(server)
           .get('/')
@@ -31,37 +26,24 @@ describe('Route integration', () => {
 
   describe('/markets', () => {
     describe('GET', () => {
-      it('responds with 200 status and application/json content type', () => {
+      xit('responds with 200 status and application/json content type', () => {
       });
 
       // For this test, you'll need to inspect the body of the response and
       // ensure it contains the markets list. Check the markets.dev.json file
       // in the dev database to get an idea of what shape you're expecting.
-      it('includes markets from "DB" json in body', () => {
+      xit('markets from "DB" json are in body of response', () => {
       });
     });
 
-    describe('POST', () => {
-      it('responds with 201 status and application/json content type', () => {
+    describe('PUT', () => {
+      xit('responds with 200 status and application/json content type', () => {
       });
 
-      // Hint: you should expect 5 fields on this object
-      it('responds with the item that was created in the DB', () => {
+      xit('responds with the updated market list', () => {
       });
 
-      it('responds to invalid request with 400 status and error message in body', () => {
-      });
-    });
-
-    describe('PATCH', () => {
-      it('responds with 200 status and application/json content type', () => {
-      });
-
-      it('responds with the item that was updated in the DB', () => {
-      });
-
-      // This feature is not yet implemented. Follow TDD!
-      it('responds to invalid request with 400 status and error message in body', () => {
+      xit('responds to invalid request with 400 status and error message in body', () => {
       });
     });
   });
--- server/index.js
+++ server/index.js
@@ -9,26 +9,12 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.resolve(__dirname, '../dist')));
 app.use(express.json());
 
-app.post('/markets', (req, res, next) => {
-  const { location, cards } = req.body;
-  if (typeof location !== 'string' || typeof cards !== 'number') {
-    return next({ code: 400, error: 'Location and cards fields required' });
+app.put('/markets', (req, res, next) => {
+  const syncResult = db.sync(req.body);
+  if (syncResult instanceof Error) {
+    return next({ code: 400, error: syncResult });
   }
-
-  const newMarket = db.create({ location, cards });
-  res.status(201).json(newMarket);
-});
-
-app.patch('/markets/:id', (req, res, next) => {
-  const { id } = req.params;
-  const market = db.findById(parseInt(id));
-  if (!market) {
-    return next({ code: 404, error: 'No market to update' });
-  }
-
-  const cards = market.cards + req.body.cards;
-  if (cards < 0) return next({ code: 400, error: 'Too low!' });
-  res.json(db.update({ id, cards }));
+  res.json(syncResult);
 });
 
 app.get('/markets', (req, res) => {
